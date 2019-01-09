4b4f0642064146886a21ac8bf95ec4abd3e185a8
JoungCNTT <46478597+JoungCNTT@users.noreply.github.com>
2019-01-09 14:04:11 +0700

    Create sanpham.php

---
+++ sanpham.php
@@ -0,0 +1,23 @@
+<?php
+	//chèn file vào 1 laanfkhoong trùng nhau
+	require_once('connect.php');
+	$link = ketnoi();
+	if(isset($_GET['madm'])) $madm = $_GET['madm'];
+	$sql ="SELECT * FROM san_pham";
+	if($madm !=0) $sql = "WHERE madm = $madm";
+	$kq = mysqli_query($link, $sql);
+	if($kq ==false || mysqli_num_rows($kq)==0) die("Chưa có danh mục");
+	while ($sp = mysqli_fetch_object($kq))
+	{
+		$anhsp = "anhsp/acer.jpg";
+		if(is_file("anhsp/".$sp->masp.'.jpg')) $anhsp = "anhsp/".$sp->masp.'.jpg'
+		?>
+        	<div class="sanpham">
+            	<div><img src="<?php echo $anhsp ?>"/></div>
+                <div><?php echo $sp->tensp; ?></div>
+                div><?php echo $sp->giasp; ?></div>
+            </div>
+        <?php
+	}
+	
+?>
